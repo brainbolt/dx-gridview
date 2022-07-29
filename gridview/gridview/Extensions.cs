d9cb5c902674c9a3181b447f2e58e7216e98f6e3
Brandon Rainbolt <brandon@brainbolt.io>
2022-07-29 11:16:17 -0700

    inti

---
+++ gridview/gridview/Extensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using DevExpress.XtraGrid.Views.Base;
+
+namespace gridview
+{
+    public static class Extensions
+    {
+        public static void DrawAdditionalItemScheduleIndicator(this RowCellCustomDrawEventArgs customDrawEventArgs)
+        {
+            customDrawEventArgs.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+
+            var rectangle = new Rectangle(customDrawEventArgs.Bounds.Right - customDrawEventArgs.Bounds.Height,
+                                          customDrawEventArgs.Bounds.Top,
+                                          customDrawEventArgs.Bounds.Height - 1, customDrawEventArgs.Bounds.Height - 1);
+            using (var graphicsPath = new GraphicsPath())
+            {
+                graphicsPath.AddEllipse(rectangle);
+                using (var brush = new PathGradientBrush(graphicsPath))
+                {
+                    brush.CenterPoint = new PointF(rectangle.Left + 3, rectangle.Top + 3);
+                    brush.CenterColor = Color.White;
+                    brush.SurroundColors = new[] { Color.LightGray };
+                    customDrawEventArgs.Graphics.FillEllipse(brush, rectangle);
+                }
+            }
+            using (var pen = new Pen(Color.WhiteSmoke) { Width = 0.25f })
+            {
+                customDrawEventArgs.Graphics.DrawEllipse(pen, rectangle);
+            }
+
+            var cellFont = customDrawEventArgs.Appearance.GetFont();
+            using (var font = new Font(cellFont.FontFamily, cellFont.Size * .8f))
+            {
+                var format = new StringFormat { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center };
+                customDrawEventArgs.Graphics.DrawString(1.ToString(), font, Brushes.Black, rectangle, format);
+            }
+        }
+        public static void PaintNonSelectedComponentScheduleCellBackground(this RowCellCustomDrawEventArgs customDrawEventArgs)
+        {
+            using (var brush = new SolidBrush(Color.FromArgb(255, 225, 225)))
+                customDrawEventArgs.Graphics.FillRectangle(brush, customDrawEventArgs.Bounds);
+        }
+    }
+}
---
+++ gridview/gridview/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.Utils;
+using DevExpress.XtraGrid.Views.Base;
+
+namespace gridview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            VisibleChanged += Form1_VisibleChanged;
+            gridView1.CustomDrawCell += ComponentScheduleGridViewOnCustomDrawCell;
+        }
+
+        private void Form1_VisibleChanged(object sender, EventArgs e)
+        {
+            if (!Visible) return;
+
+            gridControl1.DataSource = GridRow.Create(5);
+        }
+
+        void ComponentScheduleGridViewOnCustomDrawCell(object sender, RowCellCustomDrawEventArgs e)
+        {
+            e.PaintNonSelectedComponentScheduleCellBackground();
+            e.DrawAdditionalItemScheduleIndicator();
+        }
+
+        public class GridRow
+        {
+            public string Column1 { get; set; }
+
+            public GridRow()
+            {
+                Column1 = Guid.NewGuid().ToString();
+            }
+
+            public static List<GridRow> Create(int number) 
+                => Enumerable.Range(0, number)
+                    .Select(_ => new GridRow())
+                    .ToList();
+        }
+    }
+}
